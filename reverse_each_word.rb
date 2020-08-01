cc04778342c69f5e31d514166a9a64a907668468
Joshua Collins <pc.refuge@gmail.com>
2020-08-01 22:13:37 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,12 @@
+# def reverse_each_word(sentence)
+#   new_sentence = []
+#   single_word = sentence.split(" ")
+#   single_word.collect do |word|
+#     new_sentence.push(word.reverse)
+#   end
+#   return new_sentence.join(" ")
+# end
+
 def reverse_each_word(sentence)
-  new_sentence = []
-  single_word = sentence.split(" ")
-  single_word.collect do |word|
-    new_sentence.push(word.reverse)
-  end
-  return new_sentence.join(" ")
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
