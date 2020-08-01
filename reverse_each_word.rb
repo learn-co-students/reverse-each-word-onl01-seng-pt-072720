b6efe1a6df5f6e5c8fc207a4a5e66b508812a972
Joshua Collins <pc.refuge@gmail.com>
2020-08-01 22:11:52 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(sentence)
+  new_sentence = []
+  single_word = sentence.split(" ")
+  single_word.collect do |word|
+    new_sentence.push(word.reverse)
+  end
+  return new_sentence.join(" ")
+end
